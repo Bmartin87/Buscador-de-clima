92b2c2d240f2d3cbca91ec8541fc4be463172921
bmartin87 <martinmdq87@hotmail.com>
2025-06-22 16:32:19 -0300

    3 commit

--- buscador-clima/vite.config.js
+++ buscador-clima/vite.config.js
@@ -1,7 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'docs' 
-  },
-  base: '/Buscador-clima/'
+  base: '/BUSCADOR-CLIMA/', 
 })
